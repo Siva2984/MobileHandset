7eaafbfd857fa7984d776be412948d2b2c873681
Siva2984 <71598677+Siva2984@users.noreply.github.com>
2020-09-20 19:45:42 +0400

    Add files via upload
    Created base project structure with dependencies.

---
+++ mobilehandset/src/main/java/com/axiom/mobilehandset/MobilehandsetApplication.java
@@ -0,0 +1,13 @@
+package com.axiom.mobilehandset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobilehandsetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobilehandsetApplication.class, args);
+	}
+
+}
---
+++ mobilehandset/src/test/java/com/axiom/mobilehandset/MobilehandsetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axiom.mobilehandset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobilehandsetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
